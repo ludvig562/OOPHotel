fcc95c7d025b525a092edbed59187f2368d3fd35
rofo-93 <robin_forsberg93@hotmail.com>
2026-09-22 14:20:07 +0200

    Add booking price calc and update stay length feature

--- OOPHotel/HotelBooking.cs
+++ OOPHotel/HotelBooking.cs
@@ -8,18 +8,36 @@ public class HotelBooking
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 
+    private int _Length;
+
     public HotelBooking(string name, DateTime startDate, int lengthOfStayInDays)
     {
         DateTime dateString = startDate;
         GuestName = name;
         StartDate = startDate;
         EndDate = startDate.AddDays(lengthOfStayInDays);
+        _Length = lengthOfStayInDays;
     }
+
 
     public void DisplayBookingInfo()
     {
         Console.WriteLine($"Gästnamn:{GuestName}");
         Console.WriteLine($"Startdatum:{StartDate:d}");
         Console.WriteLine($"Slutdatum:{EndDate:d}");
+        Console.WriteLine($"Ditt pris blir:{CalculateBookingPrice()}");
+        
+    }
+
+    public int CalculateBookingPrice()
+    {
+        int hotelPrice = 1500 * _Length;
+        return hotelPrice;
+    }
+
+    public void UpdateBookingDate(int lengthOfStayInDay)
+    {
+        EndDate = StartDate.AddDays(lengthOfStayInDay);
+        _Length = lengthOfStayInDay;
     }
 }
--- OOPHotel/Program.cs
+++ OOPHotel/Program.cs
@@ -4,7 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        var person1 = new HotelBooking("Robin", DateTime.Now, 5);
+        Console.WriteLine("Vad heter du: ");
+        string name = Console.ReadLine();
+
+        int number = 0;
+        Console.WriteLine("Hur länge vill du stanna: ");
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Du får bara skriva i siffor, försök igen");
+        }
+        Console.WriteLine("Vilket datum vill du boka? (åååå-mm-dd): ");
+
+        
+        //DateTime parsedDate;
+        //while (!DateTime.TryParseExact(Console.ReadLine(), "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out parsedDate)) ;
+        //{
+        //    Console.WriteLine("Det måste vara ett giltigt datum, försök igen.");
+        //}
+
+
+        var person1 = new HotelBooking(name, parsedDate, number);
+        Console.WriteLine("\nBokningsinformation:");
+        person1.DisplayBookingInfo();
+
+        Console.WriteLine("Välj det nya antalet dagar du vill stanna:");
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Du får bara skriva i siffor, försök igen");
+        }
+        person1.UpdateBookingDate(number);
+        Console.WriteLine("\nUppdaterad bokningsinformation:");
         person1.DisplayBookingInfo();
     }
 }
